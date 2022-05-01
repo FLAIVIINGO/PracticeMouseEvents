35e31903ba5568ac36879d70d6e0780a1459401c
Andrew Babilonia <86068252+FLAIVIINGO@users.noreply.github.com>
2022-05-01 12:41:48 -0400

    drag events completed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import controls.Arrow;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
@@ -9,34 +10,49 @@ public class Controller {
 
     @FXML
     AnchorPane graph;
-    Vertex vertexTemp;
+    Vertex vertex1;
+    Vertex vertex2;
+    Arrow arrow;
     Vertex vertexDelete;
 
     public void onGraphPressed(MouseEvent mouseEvent) {
         if(mouseEvent.isPrimaryButtonDown())
-            createAndAddVertex(mouseEvent.getX(), mouseEvent.getY());
+            vertex1 = createAndAddVertex(mouseEvent.getX(), mouseEvent.getY());
     }
 
     public void onGraphDragDetected(MouseEvent mouseEvent) {
-        if(mouseEvent.isPrimaryButtonDown())
-            vertexTemp = createAndAddVertex(mouseEvent.getX(), mouseEvent.getY());
+        if(mouseEvent.isPrimaryButtonDown()) {
+            vertex2 = createAndAddVertex(mouseEvent.getX(), mouseEvent.getY());
+            arrow = createAndAddArrow(vertex1, vertex2);
+            vertex2.getStyleClass().add("dragged");
+            arrow.getStyleClass().add("dragged");
+        }
     }
 
     public void onGraphDragged(MouseEvent mouseEvent) {
-        if(vertexTemp != null) {
-            vertexTemp.setLayoutX(mouseEvent.getX());
-            vertexTemp.setLayoutY(mouseEvent.getY());
+        if(vertex2 != null) {
+            vertex2.setLayoutX(mouseEvent.getX());
+            vertex2.setLayoutY(mouseEvent.getY());
         }
     }
 
     public void onGraphReleased(MouseEvent mouseEvent) {
-        vertexTemp = null;
+        if (vertex2 != null) {
+            vertex2.getStyleClass().remove("dragged");
+            arrow.getStyleClass().remove("dragged");
+        }
+        vertex2 = null;
     }
 
     private Vertex createAndAddVertex(Double x, Double y) {
         Vertex vertex = new Vertex(x, y);
 
-
+        vertex.setOnAction(e -> {
+            for(Arrow a : vertex.edges) {
+                a.setHeadAVisible(!a.isHeadAVisible());
+                a.setHeadBVisible(!a.isHeadBVisible());
+            }
+        });
         vertex.setOnMousePressed(mouseEvent -> onVertexPressed(mouseEvent, vertex));
         vertex.setOnDragDetected(e -> onVertexDraggedDetected(e, vertex));
         vertex.setOnMouseDragged(e -> onVertexDragged(e, vertex));
@@ -45,31 +61,67 @@ public class Controller {
         return vertex;
     }
 
+    private Arrow createAndAddArrow(Vertex v1, Vertex v2) {
+        Arrow arrow = new Arrow(v1.getLayoutX(), v1.getLayoutY(), v2.getLayoutX(), v2.getLayoutY());
+        arrow.x1Property().bind(v1.layoutXProperty());
+        arrow.y1Property().bind(v1.layoutYProperty());
+        arrow.x2Property().bind(v2.layoutXProperty());
+        arrow.y2Property().bind(v2.layoutYProperty());
+
+        v1.edges.add(arrow);
+        v2.edges.add(arrow);
+        graph.getChildren().add(arrow);
+        return arrow;
+    }
+
     private void onVertexPressed(MouseEvent mouseEvent, Vertex vertex) {
-        vertexDelete = vertex;
+        if(mouseEvent.isPrimaryButtonDown()) {
+            vertex1 = vertex;
+        } else if(mouseEvent.isSecondaryButtonDown()) {
+            vertexDelete = vertex;
+        }
     }
 
     private void onVertexReleased(MouseEvent e, Vertex vertex) {
+        vertex.getStyleClass().remove("dragged");
+        for(Arrow a : vertex.edges) {
+            a.getStyleClass().remove("dragged");
+        }
+        if(vertex2 != null) {
+            vertex2.getStyleClass().remove("dragged");
+        }
+
         if(vertexDelete != null) {
             graph.getChildren().remove(vertexDelete);
+            for(Arrow a : vertexDelete.edges) {
+                graph.getChildren().remove(a);
+            }
         }
-        vertexTemp = null;
+        vertex2 = null;
         vertexDelete = null;
     }
 
-    private void onVertexDraggedDetected(MouseEvent e, Node vertex) {
+    private void onVertexDraggedDetected(MouseEvent e, Vertex vertex) {
         vertex.toFront();
-        vertexDelete = null;
         if(e.isPrimaryButtonDown()) {
-            vertexTemp = createAndAddVertex(vertex.getLayoutX() + e.getX() + vertex.getTranslateX(),
+            vertex2 = createAndAddVertex(vertex.getLayoutX() + e.getX() + vertex.getTranslateX(),
                                             vertex.getLayoutY() + e.getY() + vertex.getTranslateY());
+            arrow = createAndAddArrow(vertex1, vertex2);
+            vertex2.getStyleClass().add("dragged");
+            arrow.getStyleClass().add("dragged");
+        } else if(e.isSecondaryButtonDown()) {
+            vertexDelete = null;
+            vertex.getStyleClass().add("dragged");
+            for(Arrow a : vertex.edges) {
+                a.getStyleClass().add("dragged");
+            }
         }
     }
 
     private void onVertexDragged(MouseEvent e, Node vertex) {
-        if(vertexTemp != null) {
-            vertexTemp.setLayoutX(vertex.getLayoutX() + e.getX() + vertex.getTranslateX());
-            vertexTemp.setLayoutY(vertex.getLayoutY() + e.getY() + vertex.getTranslateY());
+        if(vertex2 != null) {
+            vertex2.setLayoutX(vertex.getLayoutX() + e.getX() + vertex.getTranslateX());
+            vertex2.setLayoutY(vertex.getLayoutY() + e.getY() + vertex.getTranslateY());
         }
         if(e.isSecondaryButtonDown()) {
             vertex.setLayoutX(vertex.getLayoutX() + e.getX() + vertex.getTranslateX());
--- src/sample/Vertex.java
+++ src/sample/Vertex.java
@@ -1,21 +1,24 @@
 package sample;
 
+import controls.Arrow;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
-import javafx.scene.shape.Circle;
 
 public class Vertex extends Button {
 
     private static int count = 0;
     public int id;
+    public ObservableList<Arrow> edges = FXCollections.observableArrayList();
 
     public Vertex(Double x, Double y) {
         setLayoutX(x);
         setLayoutY(y);
-        setShape(new Circle(30));
-        setPrefSize(50,40);
+
         translateXProperty().bind(widthProperty().divide(-2));
         translateYProperty().bind(heightProperty().divide(-2));
         id = count++;
         setText(id + "");
+        getStyleClass().add("visNode");
     }
 }
